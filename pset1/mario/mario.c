0354b4bae883f9c72823f8c012413ed8997c641b
aabusang <abusangadam2001@gmail.com>
2020-08-13 20:33:46 +0000

    final commit

--- pset1/mario/mario.c
+++ pset1/mario/mario.c
@@ -3,22 +3,30 @@
 
 int main(void)
 {
-    int height;
-    do
-    {
-        height = get_int("Height: ");
-    }while(height <= 0 || height > 8);
+   int n;
+   do
+   {
+        n = get_int("Height: ");
+   }while(n < 1 ||  n > 8);
 
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = height; j >= 0; j--)
-        {
-            printf("#");
-        }
-        for (int k = 0; k <= i; k++)
-        {
-            printf("*");
-        }
-        printf("\n");
-    }
+   int row, a, mid, b, c;
+   for ( row = 1; row <= n; row++)
+   {
+      for (a = n; a > row; a--) //decreasing blank triangle
+         printf(" ");
+      for (b = 1; b <= row; b++) //increasing leftward triangle
+         printf("#");
+      for (mid = 1; mid < 2; mid++) //middle space
+         printf(" ");
+      for (c = 1; c <= row; c++) //increasing rightward triangle
+         printf("#");
+
+      printf("\n");
+   }
 }
+
+// Troubles I had in this problem
+
+// I struggle to make the downward blank decreasing triangle
+      // The secret: was that I compare the "a" w/ 0 instead of no of rows
+      //
