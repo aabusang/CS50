03b07ebc2e2a0aee9722e962617c1c0cb6939c50
aabusang <abusangadam2001@gmail.com>
2020-08-29 21:14:54 +0000

    Done With Credit.c

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -6,19 +6,57 @@ void printBrand(long ccn);
 
 int main(void)
 {
-    long credCardN = get_long("Number: ");
-    // if card length is not 13 or 15 or 16
-    // printf("INVALID");
-    // Or
-    // if it does not pass the checksum
-    // printf("INVALID");
-    if (checksum(credCardN))
-        printBrand(credCardN);
+    long creditCardNunmber = get_long("Number: ");
+    if (checksum(creditCardNunmber))
+    {
+        printBrand(creditCardNunmber);
+    }
     else
+    {
         printf("INVALID\n");
+        // return 1;
+    }
 }
 
+// int ccnLen(long ccn)
+// {
+//     int len = 0;
+//     for (int i = 0; i != '\0'; i++)
+//     {
+//         len++;
+//     }
+//     return len;
+// }
 bool checksum(long ccn)
 {
+    int i, digit, sum = 0;
+    for (i = 0; ccn > 0; ccn /= 10, i++)
+    {
+        if (i % 2 == 0)
+        {
+            sum += ccn % 10;
+        }
+        else
+        {
+            digit = (ccn % 10) * 2;
+            sum += (digit / 10) + (digit % 10); //if digit = 12, / 10 = 1 & % 10 = 2
+        }
+    }
+    // int len = ccnLen(ccn);
+    if ((sum % 10 == 0) && (i == 13 || i == 15 || i == 16))
+        return true;
+    else
+        return false;
+}
 
+void printBrand(long ccn)
+{
+    if( (ccn >= 34e13 && ccn < 35e13) || (ccn >= 37e13 && ccn < 38e13) )
+        printf("AMEX\n");
+    else if (ccn >= 51e14 && ccn < 56e14)
+        printf("MASTERCARD\n");
+    else if ( (ccn >= 4e12 && ccn < 5e12) || (ccn >= 4e15 && ccn < 5e15) )
+        printf("VISA\n");
+    else
+        printf("INVALID\n");
 }
